d6a2f75260d92a7485ca894835bea216a3da62bc
MakarM97 <165716716+MakarM97@users.noreply.github.com>
2024-05-11 01:30:14 +1000

    Add files via upload

--- Перегрузка операторов.py
+++ Перегрузка операторов.py
@@ -1,21 +1,21 @@
 class Buiding:
     def __init__(self, numberOfFloors, buildingType):
-        self.numberOfFloors = int(numberOfFloors)
-        self.buildingType = str(buildingType)
-        # print(type(self.numberOfFloors))
-        # print(type(self.buildingType))
+        self.numberOfFloors = numberOfFloors
+        self.buildingType = buildingType
+
+    def __str__(self):
+        return str(self.buildingType)
+
+    def __int__(self):
+        return int(self.numberOfFloors)
 
     def __eq__(self, other):
         return self.numberOfFloors == other.buildingType
 
 
-my_building1 = Buiding(2, 'str')
-my_building2 = Buiding(4, 'string')
-print(my_building1)
-print(my_building2)
-if my_building1 == my_building2:# В коде ошибки нет но почему, если мы сравниваем инт и стр? Или я вообще неверно код написал?
-    print('Ок')# Не пойму почему этот принт не выводит???
-
-
-
-
+numberOfFloors = Buiding(10, 'str')
+buildingType = Buiding(10, 'str')
+res = numberOfFloors == buildingType
+print(res)
+if Buiding.__eq__(self=numberOfFloors, other=buildingType):
+    pass
